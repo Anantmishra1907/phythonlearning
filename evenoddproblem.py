1a47ecc6086a494da1039d1cdce3c80d5f7284f7
Anant Mishra <anantmishra1907@gmail.com>
2025-01-11 11:04:42 +0530

    This is to check whether a number is even or odd

---
+++ evenoddproblem.py
@@ -0,0 +1,7 @@
+''' Basic if Else problem to Check the given number is even or odd '''
+num = int (input ("Enter the number : "))
+if (num %2 == 0):
+    print("Given number is Even ")
+else :
+    print("Given number is Odd ")
+ 
